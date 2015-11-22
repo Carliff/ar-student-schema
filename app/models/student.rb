377de278ea653869effc90e9960d93b1a64b4d2e
Carliff <carliffrizal.carleel@gmail.com>
2015-11-22 23:36:17 +0800

    completed validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,12 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 	attr_reader :age, :name
+
 # implement your Student model here
+
+	validates :email, :format => { :with => /\w@\w{1}+[.]\w{2}/}, :uniqueness => true
+	validates :age, :numericality => {greater_than: 4}
+	validate :valid_phone?
 
 	def name
 		@name = first_name + " " + last_name
@@ -11,5 +16,11 @@ class Student < ActiveRecord::Base
 	def age
 		now = Date.today.year
  		@age = now - (birthday.year)
+	end
+
+	def valid_phone?
+		if self.phone.scan(/\d+/).join.length < 10
+			errors.add(:phone, "Must contain at least 10 digits.")
+		end
 	end
 end
