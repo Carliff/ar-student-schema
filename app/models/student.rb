ce0bd6b06d11b054e63cb6eef60b142b41951caf
Carliff <carliffrizal.carleel@gmail.com>
2015-11-22 23:25:43 +0800

    update #name and #age

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+	attr_reader :age, :name
 # implement your Student model here
+
+	def name
+		@name = first_name + " " + last_name
+	end
+
+	def age
+		now = Date.today.year
+ 		@age = now - (birthday.year)
+	end
 end
